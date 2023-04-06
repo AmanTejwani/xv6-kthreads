a259be7d07199e110ae71a1a2a0bdf9ec3c20528
SNisarg21 <shahnsiarg2121@gmail.com>
2023-04-06 17:21:47 +0530

    Added Clone_vm flag

--- clonetest.c
+++ clonetest.c
@@ -1,8 +1,7 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-// #include "kalloc.c"
-
+#define CLONE_VM 1
 int f(void *arg){
     printf(1," I am in f \n");
     return 1;
@@ -10,12 +9,10 @@ int f(void *arg){
 void clonetest(){
     int tid;
     int args=5;
-    // void *stack=(void *)kalloc();
-    tid=clone(f,(void *)0,0,(void *)args);
+    tid = clone(f, (void *)0, CLONE_VM, (void *)args); 
     printf(1,"  value received from clone is %d\n",tid);
 }
 int main(){
     clonetest();
     exit();
 }
-
