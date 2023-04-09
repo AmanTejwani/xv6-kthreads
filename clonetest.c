17eb0481d1e11c5320a96ea57d98324794f0d53a
AmanTejwani <amantejwani17@gmail.com>
2023-04-10 00:31:56 +0530

    Modified User testing program

--- clonetest.c
+++ clonetest.c
@@ -1,18 +1,31 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-#define CLONE_VM 1
-int f(void *arg){
-    printf(1," I am in f \n");
-    return 1;
-}
-void clonetest(){
-    int tid;
-    int args=5;
-    tid = clone(f, (void *)0, CLONE_VM, (void *)args); 
-    printf(1,"  value received from clone is %d\n",tid);
+#define STACK_SIZE 4096
+#define CLONE_VM   1
+int y=15;
+int func(void *arg){
+    if(*((int*)arg)==5)
+        printf(1,"Argument Received Correctly \n");
+    else
+        printf(1,"Argument Received Correctly \n");
+    y++;
+    exit();
 }
 int main(){
-    clonetest();
+    int tid;
+    int args=5;
+    char *childStack=(char *)malloc(STACK_SIZE);
+    tid=clone(func,childStack+STACK_SIZE,CLONE_VM,&args);
+    sleep(5);
+    if(CLONE_VM){
+        if(y==16){
+            printf(1," CLONE_VM  test passed\n",y);
+        }
+    }
+    int x=join(tid);
+    if(x==tid)
+        printf(1,"Joined Successfully \n");
+    sleep(5);
     exit();
 }
